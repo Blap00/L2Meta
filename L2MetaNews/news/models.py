b8c1ae2e6826a899382a5bb6857d3f75aa0eda31
Blap00 <fa.palma@duocuc.cl>
2024-07-16 12:21:31 -0400

    SetUp ENV + models

--- L2MetaNews/news/models.py
+++ L2MetaNews/news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class News(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    fechaSubida = models.DateField(default=timezone.now().date())
+
+    def __str__(self):
+        return self.title
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return f"Image for {self.news.title}"
+
