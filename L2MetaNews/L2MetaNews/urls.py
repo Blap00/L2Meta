7eeb2df98d02f9c2891df20993addb75cedd79f0
cmorenohz <cristobalalmorenohenriquez@gmail.com>
2024-07-19 20:04:17 -0400

    url4

--- L2MetaNews/L2MetaNews/urls.py
+++ L2MetaNews/L2MetaNews/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.http import HttpResponsePermanentRedirect
 
-def redirect_to_www(request):
-    return HttpResponsePermanentRedirect(f'http://www.{request.get_host()}{request.get_full_path()}')
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    re_path(r'^(?i)(?!www\.).*$', redirect_to_www),  # redirigir lo que no empiece con 'www'
     path('', include("news.urls")),
     path('admin/', admin.site.urls),
 ]
+
+
